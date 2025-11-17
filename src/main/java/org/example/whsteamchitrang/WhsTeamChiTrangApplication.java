03001076147c5487655a53f3e4f8eda00835823b
BuiVanThiet <thietbuivan2512@gmail.com>
2025-11-17 16:02:18 +0700

    ngay17112025

---
+++ src/main/java/org/example/whsteamchitrang/WhsTeamChiTrangApplication.java
@@ -0,0 +1,13 @@
+package org.example.whsteamchitrang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhsTeamChiTrangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhsTeamChiTrangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/whsteamchitrang/WhsTeamChiTrangApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.whsteamchitrang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhsTeamChiTrangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
